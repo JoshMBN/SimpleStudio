7c37d625959f892698c3b096dda2b8fa1e5402aa
JoshMBN <joshua.brennan.niu@gmail.com>
2022-05-08 20:04:00 +1200

    Fixed all errors and made menu functionable.

--- SimpleSleuth/SimpleSleuth/Program.cs
+++ SimpleSleuth/SimpleSleuth/Program.cs
@@ -3,61 +3,144 @@ using System.Threading;
 
 namespace SimpleSleuth
 {
-    internal class Program
-    {
-	
-        static void Main(string[] args)
-        {
-			public string answer = "";
-			mainMenu();
-        }
-
-		static void mainMenu()
+	internal class Program
+	{
+		static public int menuAnswer = 0;
+		static public char pageAnswer = 'a';
+		static void Main()
 		{
+
+			MainMenu();
+		}
+
+		static void MainMenu()
+		{
+			
 			Console.Clear();
 			Console.WriteLine("Welcome to the wonderful MENU!");
 			Console.WriteLine("1. Page 1.");
 			Console.WriteLine("2. Page 2.");
 			Console.WriteLine("3. Page 3.");
 			Console.WriteLine("4. Page 4.");
-	
-			answer = Console.ReadLine();
-			switch(answer)
+
+			menuAnswer = Convert.ToInt32(Console.ReadLine());
+			switch (menuAnswer)
 			{
 				case 1:
-				page1();
-				break;
-			case 2:
-				page2();
-				break;
-			case 3:
-				page3();
-				break;
-			case 4:
-				page4();
-				break;
-			default:
-				Console.Clear();
-				Console.WriteLine("Sorry that option wasn't one from the Menu!");
-				Console.WriteLine("The options were 1, 2, 3 or 4");
-				Thread.Sleep(4000);
-				mainMenu();
-				break;
+					Page1();
+					break;
+				case 2:
+					Page2();
+					break;
+				case 3:
+					Page3();
+					break;
+				case 4:
+					Page4();
+					break;
+				default:
+					Console.Clear();
+					Console.WriteLine("Sorry that option wasn't one from the Menu!");
+					Console.WriteLine("The options were 1, 2, 3 or 4");
+					Thread.Sleep(4000);
+					Main();
+					break;
 			}
 		}
 
-	static void page1(mainMenu())
-	{
-	    Console.WriteLine("Welcome to page 1!");
-	    Console.WriteLine("Why don't you sit a while and listen?");
-	    Thread.Sleep(4000);
+		static void ExitScreen()
+        {
+			Console.Clear();
+			Console.WriteLine("Thank you for sampling my menu!");
+			Console.WriteLine("Goodbye!");
+			Thread.Sleep(5000);
+        }
+
+		static void Page1()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to page 1!");
+			Console.WriteLine("Why don't you sit a while and listen?");
+			Thread.Sleep(4000);
 
 			Console.Write("Back to Main Menu? y/n: ");
 
-		answer = Console.ReadLine();
+			pageAnswer = Convert.ToChar(Console.ReadLine());
 
-		if (answer == 'y') 
-			{mainMenu()};
+			if (pageAnswer == 'y')
+			{
+				Main();
+			}
+			else if (pageAnswer == 'n')
+			{
+				ExitScreen();
+			}
+
+
+		}
+		static void Page2()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to page 2!");
+			Console.WriteLine("Why don't you sit a while and listen?");
+			Thread.Sleep(4000);
+
+			Console.Write("Back to Main Menu? y/n: ");
+
+			pageAnswer = Convert.ToChar(Console.ReadLine());
+
+			if (pageAnswer == 'y')
+			{
+				Main();
+			}
+			else if (pageAnswer == 'n')
+            {
+				ExitScreen();
+            }
+		}
+
+		static void Page3()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to page 3!");
+			Console.WriteLine("Why don't you sit a while and listen?");
+			Thread.Sleep(4000);
+
+			Console.Write("Back to Main Menu? y/n: ");
+
+			pageAnswer = Convert.ToChar(Console.ReadLine());
+
+			if (pageAnswer == 'y')
+			{
+				Main();
+			}
+			else if (pageAnswer == 'n')
+			{
+				ExitScreen();
+			}
+		}
+
+		static void Page4()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to page 4!");
+			Console.WriteLine("Why don't you sit a while and listen?");
+			Thread.Sleep(4000);
+
+			Console.Write("Back to Main Menu? y/n: ");
+
+			pageAnswer = Convert.ToChar(Console.ReadLine());
+
+			if (pageAnswer == 'y')
+			{
+				Main();
+			}
+			else if (pageAnswer == 'n')
+			{
+				ExitScreen();
+			}
+
+		}
 	}
 }
 
