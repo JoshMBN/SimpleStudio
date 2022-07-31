333eb23184c54cc90261ac2b38fd6d0db9a125d6
JoshMBN <joshua.brennan.niu@gmail.com>
2022-08-01 11:37:41 +1200

    Commit All Changes

--- DungeonCrawler/Form2.Designer.cs
+++ DungeonCrawler/Form2.Designer.cs
@@ -29,66 +29,20 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(58, 46);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(668, 412);
             this.panel1.TabIndex = 0;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(142, 298);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(65, 51);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "D";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(76, 355);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(65, 51);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "C";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(5, 298);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(65, 51);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "B";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(76, 242);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 51);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "A";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -99,6 +53,13 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(656, 231);
             this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(5, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
             // 
             // Form2
             // 
@@ -120,10 +81,7 @@
         #endregion
 
         private Panel panel1;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Button button1;
         private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
