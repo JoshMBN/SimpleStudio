72d9c05d7db4d97603aeb78f4bffcbaac6b297c0
JoshMBN <joshua.brennan.niu@gmail.com>
2022-08-09 08:01:31 +1200

    Added new game and Objects Cs files

---
+++ The Capital/Potion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Capital
+{
+    internal class Potion
+    {
+        public string Name;
+        public int Value, Cost;
+
+        public Potion(string Name, int Value, int Cost)
+        {
+            this.Name = Name;
+            this.Value = Value;
+            this.Cost = Cost;
+        }
+
+    }
+}
---
+++ The Capital/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace The_Capital
+{
+    public class Weapon
+    {
+        public string Name;
+        public int Value, Cost;
+
+        public Weapon(string Name, int Value, int Cost)
+        {
+            this.Name = Name;
+            this.Value = Value;
+            this.Cost = Cost;
+        }
+
+    }
+}
